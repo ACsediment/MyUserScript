ed5d5b93943d180d42e068d19b5a19015ea162e3
ACsediment <ACsediment@users.noreply.github.com>
2017-03-20 02:39:24 +0800

    bug fix, enforce https
    修复无法匹配其他cache模式的bug，强制https

--- google/GoogleCacheBrowser.user.js
+++ google/GoogleCacheBrowser.user.js
@@ -7,16 +7,19 @@
 // @include     https://webcache.googleusercontent.com/search?*
 // @version 0.0.1.20140517140355
 // ==/UserScript==
-// fork from muzuiget https://greasyfork.org/zh-CN/scripts/1108-google-cache-browser
+// fork from @muzuiget https://greasyfork.org/zh-CN/scripts/1108-google-cache-browser
 
 var convertCacheLinks = function(url) {
-    var selector = 'body > div[style="position:relative;"] a';
+    var selector = 'body > div[style^="position:relative;"] a'; //更新：此处更改为“匹配字段开头”以适应更多情况。use ^= instead.
     var links = document.querySelectorAll(selector);
     var i, link;
     console.log(links.length);
     for (i = 0; i < links.length; i += 1) {
         link = links[i];
-        link.href = location.href.replace(url, encodeURIComponent(link.href));
+        link.href = location.href.replace(url, encodeURIComponent(link.href)); 
+        //@muzuiget采用做法的好处是保留当前的cache参数不变。如果需要强制指定为纯文本模式，可以改成直接生成链接
+        link.href = link.href.replace('http://webcache.googleusercontent.com/','https://webcache.googleusercontent.com/'); 
+        //更新：enforce https. 强制https，对非https的网站，脚本默认不会加载，效果类似纯文本
     }
 };
 
