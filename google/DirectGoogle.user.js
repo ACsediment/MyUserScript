04118b0446c265e253f95b7e785ce77244b4c99d
ACsediment <ACsediment@users.noreply.github.com>
2017-06-01 22:10:34 +0800

    [cache link] switch to paint text mode
    switch cache link to paint text mode

--- google/DirectGoogle.user.js
+++ google/DirectGoogle.user.js
@@ -2,7 +2,7 @@
 // @name         Direct Google
 // @namespace    http://userscripts.org/users/92143
 // @version      2.3
-// @description  (移除谷歌重定向并解析出缓存链接)Removes Google redirects and exposes "Cached" links. 
+// @description  (移除谷歌重定向并解析出缓存链接)Removes Google redirects and exposes "Cached" links.
 // @include      /^https?\:\/\/(www|news|maps|docs|cse|encrypted)\.google\./
 // @author       zanetu
 // @license      GPL version 2 or any later version; http://www.gnu.org/licenses/gpl-2.0.txt
@@ -11,116 +11,117 @@
 // @run-at       document-end
 // ==/UserScript==
 
-var hostname = location.hostname
-var pathname = location.pathname
-var href = location.href
+var hostname = location.hostname;
+var pathname = location.pathname;
+var href = location.href;
 
 String.prototype.contains = function(s) {
-	return -1 !== this.indexOf(s)
-}
+    return -1 !== this.indexOf(s);
+};
 
 String.prototype.startsWith = function(s) {
-	return this.slice(0, s.length) == s
-}
+    return this.slice(0, s.length) == s;
+};
 
 function stopBubbling(event) {
-	event.stopPropagation()
+    event.stopPropagation();
 }
 
 function blockListeners(element, events) {
-	if(!(element instanceof EventTarget && typeof events === 'string')) {
-		return
-	}
-	var eventList = events.split(/\W+/) || []
-	for(var i = 0, event; event = eventList[i]; i++) {
-		//removeEventListener is not needed as duplicate listeners would be discarded
-		element.addEventListener(event, stopBubbling, true)
-	}
+    if(!(element instanceof EventTarget && typeof events === 'string')) {
+        return;
+    }
+    var eventList = events.split(/\W+/) || [];
+    for(var i = 0, event; event = eventList[i]; i++) {
+        //removeEventListener is not needed as duplicate listeners would be discarded
+        element.addEventListener(event, stopBubbling, true);
+    }
 }
 
 function modifyGoogle() {
-	//remove web/video search redirects
-	$('a[onmousedown^="return rwt("]').removeAttr('onmousedown')
-	//remove custom search redirects
-	$('.gsc-results a[href][data-cturl]').each(function() {
-		blockListeners(this, 'mousedown')
-	})
-	//remove image search redirects
-	$('a').filter('[class^="irc_"], [class*=" irc_"], [id^="irc_"]').each(function() {
-		blockListeners(this, 'mousedown')
-	})
-	//remove news search redirects
-	if(href.contains('tbm=nws') || hostname.startsWith('news.google.')) {
-		$('a.article[href^="http"]').each(function() {
-			blockListeners(this, 'click contextmenu mousedown mousemove')
-		})
-	}
-	//remove shopping search redirects
-	else if(href.contains('tbm=shop') || pathname.startsWith('/shopping/')) {
-		$('a').filter('[href*="/aclk?"], [href*="/url?"]').each(function() {
-			var m = this.href.match(/(?:\&adurl|\?q|\&url)\=(http.*?)(\&|$)/i)
-			if(m && m[1]) {
-				var link = decodeURIComponent(m[1])
-				link = link.replace('=http://clickserve.dartsearch.net/', '=')
-				m = link.match(/\=(https?(\%3A\%2F\%2F|\:\/\/).*?)(\&|$)/i)
-				if(m && m[1]) {
-					link = decodeURIComponent(m[1])
-				}
-				this.href = link
-			}
-		})
-	}
-	//remove map search redirects; does not remove redirects of advertisement
-	else if(pathname.startsWith('/maps/') || '/maps' == pathname) {
-		$('a[href^="http"]').each(function() {
-			blockListeners(this, 'click contextmenu')
-			//legacy
-			if(this.href.contains('url?')) {
-				var m = this.href.match(/(?:\&|\?)q\=(http.*?)(\&|$)/i)
-				if(m && m[1]) {
-					this.href = decodeURIComponent(m[1])
-				}
-			}
-		})
-	}
-	//remove legacy search redirects and docs redirects
-	//should be done last as shopping uses the same url pattern
-	$('a[href*="/url?"]').each(function() {
-		var m = this.href.match(/\/url\?(?:url|q)\=(http.*?)(\&|$)/i)
-		if(m && m[1]) {
-			this.href = decodeURIComponent(m[1])
-		}
-	})
-	//expose cached links
-	$('div[role="menu"] ol li').find('a[href^="http://webcache.googleusercontent."]' + 
-		', a[href^="https://webcache.googleusercontent."]').each(
-		function() {
-			this.style.display = 'inline'
-			$(this).closest('div.action-menu.ab_ctl, div._nBb')
-			.after(' <a href="' + this.href.replace(/^http\:/, 'https:') + 
-				'">(https)</a> ')
-			.after($(this))
-		}
-	)
+    //remove web/video search redirects
+    $('a[onmousedown^="return rwt("]').removeAttr('onmousedown');
+    //remove custom search redirects
+    $('.gsc-results a[href][data-cturl]').each(function() {
+        blockListeners(this, 'mousedown');
+    });
+    //remove image search redirects
+    $('a').filter('[class^="irc_"], [class*=" irc_"], [id^="irc_"]').each(function() {
+        blockListeners(this, 'mousedown');
+    });
+    //remove news search redirects
+    if(href.contains('tbm=nws') || hostname.startsWith('news.google.')) {
+        $('a.article[href^="http"]').each(function() {
+            blockListeners(this, 'click contextmenu mousedown mousemove');
+        });
+    }
+    //remove shopping search redirects
+    else if(href.contains('tbm=shop') || pathname.startsWith('/shopping/')) {
+        $('a').filter('[href*="/aclk?"], [href*="/url?"]').each(function() {
+            var m = this.href.match(/(?:\&adurl|\?q|\&url)\=(http.*?)(\&|$)/i);
+            if(m && m[1]) {
+                var link = decodeURIComponent(m[1]);
+                link = link.replace('=http://clickserve.dartsearch.net/', '=');
+                m = link.match(/\=(https?(\%3A\%2F\%2F|\:\/\/).*?)(\&|$)/i);
+                if(m && m[1]) {
+                    link = decodeURIComponent(m[1]);
+                }
+                this.href = link;
+            }
+        });
+    }
+    //remove map search redirects; does not remove redirects of advertisement
+    else if(pathname.startsWith('/maps/') || '/maps' == pathname) {
+        $('a[href^="http"]').each(function() {
+            blockListeners(this, 'click contextmenu');
+            //legacy
+            if(this.href.contains('url?')) {
+                var m = this.href.match(/(?:\&|\?)q\=(http.*?)(\&|$)/i);
+                if(m && m[1]) {
+                    this.href = decodeURIComponent(m[1]);
+                }
+            }
+        });
+    }
+    //remove legacy search redirects and docs redirects
+    //should be done last as shopping uses the same url pattern
+    $('a[href*="/url?"]').each(function() {
+        var m = this.href.match(/\/url\?(?:url|q)\=(http.*?)(\&|$)/i);
+        if(m && m[1]) {
+            this.href = decodeURIComponent(m[1]);
+        }
+    });
+    //expose cached links
+    $('div[role="menu"] ol li').find('a[href^="http://webcache.googleusercontent."]' +
+                                     ', a[href^="https://webcache.googleusercontent."]').each(
+        function() {
+            this.style.display = 'inline';
+            $(this).closest('div.action-menu.ab_ctl, div._nBb')
+                .after(' <a href="' + this.href.replace(/^http\:/, 'https:') +
+                       '&strip=1">(https)</a> ') //加入“&strip=1”后为纯文本模式
+                .after($(this));
+            //this.href=this.href.replace('+&cd=1&hl=en&ct=clnk','&strip=1')
+        }
+    );
 }
 
-MutationObserver = window.MutationObserver || window.WebKitMutationObserver
+MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 if(MutationObserver) {
-	var observer = new MutationObserver(function(mutations) {
-		modifyGoogle()
-	})
-	//tiny delay needed for firefox
-	setTimeout(function() {
-		observer.observe(document.body, {
-			childList: true, 
-			subtree: true
-		})
-		modifyGoogle()
-	}, 100)
+    var observer = new MutationObserver(function(mutations) {
+        modifyGoogle();
+    });
+    //tiny delay needed for firefox
+    setTimeout(function() {
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+        modifyGoogle();
+    }, 100);
 }
 //for chrome v18-, firefox v14-, internet explorer v11-, opera v15- and safari v6-
 else {
-	setInterval(function() {
-		modifyGoogle()
-	}, 500)
+    setInterval(function() {
+        modifyGoogle();
+    }, 500);
 }
