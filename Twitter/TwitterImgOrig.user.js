ec406994a7a1872b1c0e5b51e048b50f4623498f
ACsediment <ACsediment@users.noreply.github.com>
2020-02-18 23:34:06 +0000

    Update TwitterImgOrig.user.js
    twitter改了图片地址，因此更新

--- Twitter/TwitterImgOrig.user.js
+++ Twitter/TwitterImgOrig.user.js
@@ -1,26 +1,27 @@
 // ==UserScript==
-// @name         Twitter.img:orig
+// @name         Twitter orig img
 // @namespace    https://github.com/ACsediment/MyUserScript
-// @version      0.1
-// @description  重定向推特照片到原图
+// @version      0.2
+// @description  重定向推特照片到原图尺寸
 // @include      https://pbs.twimg.com/media/*
 // @include      http://pbs.twimg.com/media/*
 // @author       Acsediment
 // @license      GPL version 2 or any later version; http://www.gnu.org/licenses/gpl-2.0.txt
 // ==/UserScript==
 
-var PathName = location.pathname;
+var pathName=location.pathname;
+var currentHref = location.href;
+var newHref;
 
-if (PathName.endsWith(":orig") === false) {
-    var Href="";
-    if (PathName.endsWith("jpg") === false) {
-        PathName = PathName.substring(0,PathName.search(':'));
-        Href = 'https://pbs.twimg.com' + PathName + ':orig';
-        location.replace(Href);
-        //对于带有图片尺寸参数的URL，修改参数为“:orig”
+if (currentHref.endsWith("orig") === false) {
+    if (currentHref.endsWith("jpg") === false) {
+        //pathName = pathName.substring(0,pathName.search(':'));
+        newHref = 'https://pbs.twimg.com' + pathName + '?format=jpg&name=orig';
+        location.replace(newHref);
+        //对于带有图片尺寸参数的URL，修改参数为“&name=orig”
     } else {
-        Href = 'https://pbs.twimg.com' + PathName + ':orig';
-        location.replace(Href);
-        //对于“.jpg”结尾的URL，直接添加“:orig”参数
+        newHref = 'https://pbs.twimg.com' + pathName + '?format=jpg&name=orig';
+        location.replace(newHref);
+        //对于“.jpg”结尾的URL，直接添加参数
     }
 }
